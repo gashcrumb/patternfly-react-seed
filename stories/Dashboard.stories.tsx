4069acee76e5dd751eb15032ae3b60371d95f41e
Stan Lewis <gashcrumb@gmail.com>
2023-08-02 15:11:55 -0400

    fix: SCO-20 update storybook
    This change updates storybook to the latest version and applies several
    migration scripts that assist with this.  However the existing stories
    need to be updated, and it would be better to move them to be closer to
    the components they document.  With this commit storybook builds,
    however the development mode does not work yet.

--- stories/Dashboard.stories.tsx
+++ stories/Dashboard.stories.tsx
@@ -1,6 +1,5 @@
-import React, { ComponentProps } from 'react';
+import React from 'react';
 import { Dashboard } from '@app/Dashboard/Dashboard';
-import { Story } from '@storybook/react';
 
 //👇 This default export determines where your story goes in the story list
 export default {
@@ -9,7 +8,7 @@ export default {
 };
 
 //👇 We create a “template” of how args map to rendering
-const Template: Story<ComponentProps<typeof Dashboard>> = (args) => <Dashboard {...args} />;
+const Template = (args) => <Dashboard {...args} />;
 
 export const FirstStory = Template.bind({});
 FirstStory.args = {
--- stories/Support.stories.tsx
+++ stories/Support.stories.tsx
@@ -1,6 +1,5 @@
-import React, { ComponentProps } from 'react';
+import React from 'react';
 import { Support } from '@app/Support/Support';
-import { Story } from '@storybook/react';
 
 //👇 This default export determines where your story goes in the story list
 export default {
@@ -9,7 +8,7 @@ export default {
 };
 
 //👇 We create a “template” of how args map to rendering
-const Template: Story<ComponentProps<typeof Support>> = (args) => <Support {...args} />;
+const Template = (args) => <Support {...args} />;
 
 export const SupportStory = Template.bind({});
 SupportStory.args = {
